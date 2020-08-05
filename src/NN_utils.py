1a32c6121cbd0538175e27f52f360881861e75e2
cailigd <cailigd@qq.com>
2020-08-05 04:03:19 -0400

    the first runnable custom hybrid model

--- src/NN_utils.py
+++ src/NN_utils.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-
+from sklearn.preprocessing import LabelEncoder
 from sklearn import metrics, calibration
 
 from evaluation import f3mer_comp, f5mer_comp, f7mer_comp
@@ -65,6 +65,219 @@ class seqDataset(Dataset):
     def __len__(self):
         return self.len
 
+class CombinedDataset(Dataset):
+    """ Diabetes dataset."""
+
+    def __init__(self,local_dataset, distal_dataset):
+        
+        self.y = local_dataset.y
+        self.local_cont_X = local_dataset.cont_X
+        self.local_cat_X = local_dataset.cat_X
+        self.distal_X = distal_dataset.x_data
+        self.len=len(self.y)
+
+    def __getitem__(self, index):
+        return self.y[index], self.local_cont_X[index], self.local_cat_X[index], self.distal_X[index]
+
+    def __len__(self):
+        return self.len
+    
+def gen_ohe_dataset(data):
+    seq_data = data['seq']
+    y_data = data['mut_type'].astype(np.float32).values.reshape(-1, 1)
+
+    seqs_ohe = seqs2ohe(seq_data, 6)
+
+    dataset = seqDataset([seqs_ohe, y_data])
+    #print(dataset[0:2][0][0][0:4,4:10])
+    
+    return dataset
+    
+def separate_local_distal(data, radius = 5): 
+    seq_len = len(data['seq'][0])
+    mid_pos = int((seq_len+1)/2)
+
+    adj_seq = pd.DataFrame([list(el[mid_pos-(radius+1):mid_pos+radius]) for el in data['seq']])
+    adj_seq.columns = ['us'+str(radius - i)for i in range(radius)] + ['mid'] + ['ds'+str(i+1)for i in range(radius)]
+
+    #local sequences and functional genomic data
+    data_local = pd.concat([adj_seq, data.drop(['pos','seq'], axis=1)], axis=1)
+
+    #consider more distal sequences
+    data_distal = data[['seq', 'mut_type']]
+    
+    categorical_features = list(adj_seq.columns)
+    #categorical_features = ["us5", "us4", "us3", "us2", "us1", "ds1", "ds2", "ds3", "ds4", "ds5"]
+    
+    return data_local, data_distal, categorical_features
+
+    
+class TabularDataset(Dataset):
+    def __init__(self, data, cat_cols, output_col):
+        """
+        Characterizes a Dataset for PyTorch
+
+        Parameters
+        ----------
+
+        data: pandas data frame
+            The data frame object for the input data. It must
+            contain all the continuous, categorical and the
+            output columns to be used.
+
+        cat_cols: List of strings
+            The names of the categorical columns in the data.
+            These columns will be passed through the embedding
+            layers in the model. These columns must be
+            label encoded beforehand. 
+
+        output_col: string
+            The name of the output variable column in the data
+            provided.
+        """
+        #first, change labels to digits
+        label_encoders = {}
+        for cat_col in cat_cols:
+            label_encoders[cat_col] = LabelEncoder()
+            data[cat_col] = label_encoders[cat_col].fit_transform(data[cat_col])
+        
+        self.n = data.shape[0]
+
+        if output_col:
+            self.y = data[output_col].astype(np.float32).values.reshape(-1, 1)
+        else:
+            self.y =    np.zeros((self.n, 1))
+
+        self.cat_cols = cat_cols
+        self.cont_cols = [col for col in data.columns if col not in self.cat_cols + [output_col]]
+
+        if self.cont_cols:
+            self.cont_X = data[self.cont_cols].astype(np.float32).values
+        else:
+            self.cont_X = np.zeros((self.n, 1))
+
+        if len(self.cat_cols) >0:
+            self.cat_X = data[cat_cols].astype(np.int64).values
+        else:
+            self.cat_X =    np.zeros((self.n, 1))
+
+    def __len__(self):
+        """
+        Denotes the total number of samples.
+        """
+        return self.n
+
+    def __getitem__(self, idx):
+        """
+        Generates one sample of data.
+        """
+        return [self.y[idx], self.cont_X[idx], self.cat_X[idx]]
+
+
+class FeedForwardNN(nn.Module):
+
+    def __init__(self, emb_dims, no_of_cont, lin_layer_sizes,
+                             output_size, emb_dropout, lin_layer_dropouts):
+
+        """
+        Parameters
+        ----------
+
+        emb_dims: List of two element tuples
+            This list will contain a two element tuple for each
+            categorical feature. The first element of a tuple will
+            denote the number of unique values of the categorical
+            feature. The second element will denote the embedding
+            dimension to be used for that feature.
+
+        no_of_cont: Integer
+            The number of continuous features in the data.
+
+        lin_layer_sizes: List of integers.
+            The size of each linear layer. The length will be equal
+            to the total number
+            of linear layers in the network.
+
+        output_size: Integer
+            The size of the final output.
+
+        emb_dropout: Float
+            The dropout to be used after the embedding layers.
+
+        lin_layer_dropouts: List of floats
+            The dropouts to be used after each linear layer.
+        """
+
+        super(FeedForwardNN, self).__init__()
+
+        # Embedding layers
+        self.emb_layers = nn.ModuleList([nn.Embedding(x, y)
+                                                                         for x, y in emb_dims])
+
+        no_of_embs = sum([y for x, y in emb_dims])
+        self.no_of_embs = no_of_embs
+        self.no_of_cont = no_of_cont
+
+        # Linear Layers
+        first_lin_layer = nn.Linear(self.no_of_embs + self.no_of_cont,
+                                                                lin_layer_sizes[0])
+
+        self.lin_layers =\
+         nn.ModuleList([first_lin_layer] +\
+                    [nn.Linear(lin_layer_sizes[i], lin_layer_sizes[i + 1])
+                     for i in range(len(lin_layer_sizes) - 1)])
+        
+        for lin_layer in self.lin_layers:
+            nn.init.kaiming_normal_(lin_layer.weight.data)
+
+        # Output Layer
+        self.output_layer = nn.Linear(lin_layer_sizes[-1],
+                                                                    output_size)
+        nn.init.kaiming_normal_(self.output_layer.weight.data)
+
+        # Batch Norm Layers
+        self.first_bn_layer = nn.BatchNorm1d(self.no_of_cont)
+        self.bn_layers = nn.ModuleList([nn.BatchNorm1d(size)
+                                                                        for size in lin_layer_sizes])
+
+        # Dropout Layers
+        self.emb_dropout_layer = nn.Dropout(emb_dropout)
+        self.droput_layers = nn.ModuleList([nn.Dropout(size)
+                                                                    for size in lin_layer_dropouts])
+
+    def forward(self, cont_data, cat_data):
+
+        if self.no_of_embs != 0:
+            x = [emb_layer(cat_data[:, i])
+                     for i,emb_layer in enumerate(self.emb_layers)]
+            ###
+        #print(len(x)) #x is a list, x[i].shape: batch_size * emb_size  
+        #print(x)
+            ###
+        x = torch.cat(x, 1) #x.shape: batch_size * sum(emb_size)
+        x = self.emb_dropout_layer(x)
+
+        if self.no_of_cont != 0:
+            normalized_cont_data = self.first_bn_layer(cont_data)
+
+            if self.no_of_embs != 0:
+                x = torch.cat([x, normalized_cont_data], 1) 
+            else:
+                x = normalized_cont_data
+
+        for lin_layer, dropout_layer, bn_layer in\
+                zip(self.lin_layers, self.droput_layers, self.bn_layers):
+            
+            x = F.relu(lin_layer(x))
+            x = bn_layer(x)
+            x = dropout_layer(x)
+
+        #x = self.output_layer(x) #oringial output
+        
+        x = torch.sigmoid(self.output_layer(x)) # for logistic regression
+
+        return x
+    
 def load_data(data_file):
     data = pd.read_csv(data_file, sep='\t').dropna()
     seq_data = data['sequence']
@@ -78,56 +291,118 @@ def load_data(data_file):
     return dataset
 
 class Network(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, RNN_hidden_size, RNN_layers, lin_layer_size):
+    def __init__(self,  emb_dims, no_of_cont, lin_layer_sizes, emb_dropout, lin_layer_dropouts, in_channels, out_channels, kernel_size, RNN_hidden_size, RNN_layers, last_lin_size):
         super(Network, self).__init__()
         #self.cnn = nn.Conv1d(in_channels, out_channels, kernel_size)
         #self.maxpool =  nn.MaxPool1d(kernel_size, stride)
         
+        #############################
+        # Embedding layers
+        self.emb_layers = nn.ModuleList([nn.Embedding(x, y) for x, y in emb_dims])
+
+        no_of_embs = sum([y for x, y in emb_dims])
+        self.no_of_embs = no_of_embs
+        self.no_of_cont = no_of_cont
+
+        # Linear Layers
+        first_lin_layer = nn.Linear(self.no_of_embs + self.no_of_cont, lin_layer_sizes[0])
+
+        self.lin_layers = nn.ModuleList([first_lin_layer] + [nn.Linear(lin_layer_sizes[i], lin_layer_sizes[i + 1]) for i in range(len(lin_layer_sizes) - 1)])
+        
+        for lin_layer in self.lin_layers:
+            nn.init.kaiming_normal_(lin_layer.weight.data)
+
+        # Output Layer
+        # self.output_layer = nn.Linear(lin_layer_sizes[-1], output_size)
+        # nn.init.kaiming_normal_(self.output_layer.weight.data)
+
+        # Batch Norm Layers
+        self.first_bn_layer = nn.BatchNorm1d(self.no_of_cont)
+        self.bn_layers = nn.ModuleList([nn.BatchNorm1d(size) for size in lin_layer_sizes])
+
+        # Dropout Layers
+        self.emb_dropout_layer = nn.Dropout(emb_dropout)
+        self.droput_layers = nn.ModuleList([nn.Dropout(size) for size in lin_layer_dropouts])
+
+        ######################################
         self.kernel_size = kernel_size
         self.RNN_hidden_size = RNN_hidden_size
         self.RNN_layers = RNN_layers
         
+        # CNN layers
         self.conv = nn.Sequential(
             nn.Conv1d(in_channels, out_channels, kernel_size), # in_channels, out_channels, kernel_size
             nn.ReLU(),
-            nn.MaxPool1d(3, 1), # kernel_size, stride
+            nn.MaxPool1d(4, 2), # kernel_size, stride
             nn.Conv1d(out_channels, out_channels*2, kernel_size),
             nn.ReLU(),
-            nn.MaxPool1d(3, 1)
+            nn.MaxPool1d(4, 2)
         )
         
+        # RNN layers
         self.rnn = nn.LSTM(out_channels*2, RNN_hidden_size, num_layers=RNN_layers, bidirectional=True)
         
+        
+        # FC layers
+        fc_in_size = RNN_hidden_size*2 + lin_layer_sizes[-1]
         self.fc = nn.Sequential(
-            nn.BatchNorm1d(RNN_hidden_size*2),
-            nn.Linear(RNN_hidden_size*2, lin_layer_size), 
+            nn.BatchNorm1d(fc_in_size),
+            nn.Linear(fc_in_size, last_lin_size), 
             nn.ReLU(),
             nn.Dropout(0.1), #dropout prob
             #nn.Linear(out_channels*2, 1),
-            nn.Linear(lin_layer_size, 1),
+            nn.Linear(last_lin_size, 1),
             #nn.ReLU(), #NOTE: addting this makes two early convergence
-            nn.Dropout(0.1)
+            #nn.Dropout(0.1)
         )
         
         #nn.init.kaiming_normal_(self.fc.weight.data) #
     
-    def forward(self, input_data):
+    def forward(self, local_input, distal_input):
+        
+        ################################
+        cont_data, cat_data = local_input
+        if self.no_of_embs != 0:
+            local_out = [emb_layer(cat_data[:, i]) for i,emb_layer in enumerate(self.emb_layers)]
+            ###
+        #print(len(x)) #x is a list, x[i].shape: batch_size * emb_size  
+        print('cont_data.shape:')
+        print(cont_data.shape)
+            ###
+        local_out = torch.cat(local_out, dim = 1) #x.shape: batch_size * sum(emb_size)
+        local_out = self.emb_dropout_layer(local_out)
+
+        if self.no_of_cont != 0:
+            normalized_cont_data = self.first_bn_layer(cont_data)
+
+            if self.no_of_embs != 0:
+                local_out = torch.cat([local_out, normalized_cont_data], dim = 1) 
+            else:
+                local_out = normalized_cont_data
+        
+        for lin_layer, dropout_layer, bn_layer in zip(self.lin_layers, self.droput_layers, self.bn_layers):
+            local_out = F.relu(lin_layer(local_out))
+            local_out = bn_layer(local_out)
+            local_out = dropout_layer(local_out)
+        ################################
+        
+        
         #input data shape: batch_size, in_channels, L_in (lenth of sequence)
-        out = self.conv(input_data) #out_shape: batch_size, L_out; L_out = floor((L_in+2*padding-kernel_size)/stride + 1)
+        distal_out = self.conv(distal_input) #out_shape: batch_size, L_out; L_out = floor((L_in+2*padding-kernel_size)/stride + 1)
         #out, _ = torch.max(out, dim=2)
         #print("out.shape")
         #print(out.shape)
-        #print(out[0:5])        
-        
-        #RNN
-        out = out.permute(2,0,1)
-        out, _ = self.rnn(out) #output of shape (seq_len, batch, num_directions * hidden_size)
-        Fwd_RNN=out[-1, :, :self.RNN_hidden_size] #output of last position
-        Rev_RNN=out[0, :, self.RNN_hidden_size:]
-        out = torch.cat((Fwd_RNN, Rev_RNN), 1)
-        print("RNN out.shape")
-        print(out.shape)        
 
+        #RNN
+        distal_out = distal_out.permute(2,0,1)
+        distal_out, _ = self.rnn(distal_out) #output of shape (seq_len, batch, num_directions * hidden_size)
+        Fwd_RNN=distal_out[-1, :, :self.RNN_hidden_size] #output of last position
+        Rev_RNN=distal_out[0, :, self.RNN_hidden_size:]
+        distal_out = torch.cat([Fwd_RNN, Rev_RNN], dim=1)
+        print("RNN out.shape")
+        print(distal_out.shape)        
+
+        out = torch.cat([local_out, distal_out], dim=1)
         
         out = self.fc(out)
         
--- src/test_NN_hybrid.py
+++ src/test_NN_hybrid.py
@@ -1,23 +1,15 @@
 import sys
-
 from sklearn.preprocessing import LabelEncoder
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-
-
 import pandas as pd
 import numpy as np
 
 from sklearn import metrics, calibration
-
-from pytorch_tabular import TabularDataset, FeedForwardNN
-
-from temperature_scaling import ModelWithTemperature, _ECELoss
-
+from NN_utils import *
+#from temperature_scaling import ModelWithTemperature, _ECELoss
 from evaluation import f3mer_comp, f5mer_comp, f7mer_comp
 
 def to_np(tensor):
@@ -46,57 +38,45 @@ data = pd.read_csv(train_file).dropna()
 
 data_test = pd.read_csv(test_file).dropna()
 
-#categorical_features = ["MSSubClass", "MSZoning", "Street", "LotShape", "YearBuilt"]
-categorical_features = ["us5", "us4", "us3", "us2", "us1", "ds1", "ds2", "ds3", "ds4", "ds5", "tdist"]
-#categorical_features = ["us5", "us4", "us3", "us2", "us1", "ds1", "ds2", "ds3", "ds4", "ds5"]
-
 output_feature = "mut_type"
 
-#Encode categorical features
-from sklearn.preprocessing import LabelEncoder
-label_encoders = {}
-for cat_col in categorical_features:
-    label_encoders[cat_col] = LabelEncoder()
-    data[cat_col] = label_encoders[cat_col].fit_transform(data[cat_col])
+data_local, data_distal, categorical_features = separate_local_distal(data, radius=5)
+data_local_test, data_distal_test, categorical_features = separate_local_distal(data_test, radius=5)
 
-dataset = TabularDataset(data=data, cat_cols=categorical_features, output_col=output_feature)
+print('categorical_features:')
+print(categorical_features)
 
-#split data into train, validation and test data
-#train_set, val_set, test_set = data.random_split(dataset, (800, 100, 100))
+dataset_local = TabularDataset(data=data_local, cat_cols=categorical_features, output_col=output_feature)
+dataset_local_test = TabularDataset(data=data_local_test, cat_cols=categorical_features, output_col=output_feature)
 
-#Encode categorical features for testing data
-label_encoders = {}
-for cat_col in categorical_features:
-    label_encoders[cat_col] = LabelEncoder()
-    data_test[cat_col] = label_encoders[cat_col].fit_transform(data_test[cat_col])
+dataset_distal = gen_ohe_dataset(data_distal)
+dataset_distal_test = gen_ohe_dataset(data_distal_test)
 
-#decoder
-#label_encoders['us5'].inverse_transform([int(i) for i in data.iloc[0, 0:10]]).tolist()
-
-dataset_test = TabularDataset(data=data_test, cat_cols=categorical_features, output_col=output_feature)
+dataset = CombinedDataset(dataset_local, dataset_distal)
+dataset_test = CombinedDataset(dataset_local_test, dataset_distal_test)
 
 #DataLoader for testing data
-dataloader1 = DataLoader(dataset_test, batch_size=len(dataset_test), shuffle=False, num_workers=1)
+dataloader1 = DataLoader(dataset_test, batch_size=1000, shuffle=False, num_workers=1)
+#test_y, test_cont_x, test_cat_x, test_distal_x = dataset_test.y, dataset_test.local_cont_X, dataset_test.local_cat_X, dataset_test.distal_X
+#test_y, test_cont_x, test_cat_x, test_distal_x = next(iter(dataloader1))
 
-test_y, test_cont_x, test_cat_x = next(iter(dataloader1))
-
-test_cont_x = test_cont_x.to(device)
-test_cat_x = test_cat_x.to(device)
-test_y = test_y.to(device)
 
 #####
 
 #DataLoader for the train data
-batchsize = 300
+batchsize = 500
+
 dataloader = DataLoader(dataset, batchsize, shuffle=True, num_workers=1)
 
-dataloader2 = DataLoader(dataset, len(dataset), shuffle=False, num_workers=1)
-all_y, all_cont_x, all_cat_x = next(iter(dataloader2))
-all_cat_x = all_cat_x.to(device)
-all_cont_x = all_cont_x.to(device)
-all_y  = all_y.to(device)
+dataloader2 = DataLoader(dataset, batch_size=1000, shuffle=False, num_workers=1)
+#all_y, all_cont_x, all_cat_x, all_distal_x = next(iter(dataloader2))
+#all_y, all_cont_x, all_cat_x, all_distal_x = dataset.y, dataset.local_cont_X, dataset.local_cat_X, dataset.distal_X
+#all_cont_x = all_cont_x.to(device)
+#all_cat_x = all_cat_x.to(device)
+#all_distal_x = all_distal_x.to(device)
+#all_y = all_y.to(device)
 
-cat_dims = [int(data[col].nunique()) for col in categorical_features]
+cat_dims = [int(data_local[col].nunique()) for col in categorical_features]
 #cat_dims
 #[15, 5, 2, 4, 112]
 
@@ -107,11 +87,13 @@ emb_dims = [(x, min(50, (x + 1) // 2)) for x in cat_dims]
 # define the model 
 #model = FeedForwardNN(emb_dims, no_of_cont=15, lin_layer_sizes=[50, 100], output_size=1, emb_dropout=0.04, lin_layer_dropouts=[0.001,0.01]).to(device)
 #model = FeedForwardNN(emb_dims, no_of_cont=15, lin_layer_sizes=[50, 200], output_size=1, emb_dropout=0.001, lin_layer_dropouts=[0.001,0.001]).to(device) #bs=8000
-model = FeedForwardNN(emb_dims, no_of_cont=n_cont, lin_layer_sizes=[200, 100], output_size=1, emb_dropout=0.3, lin_layer_dropouts=[0.1,0.1]).to(device)
+#model = FeedForwardNN(emb_dims, no_of_cont=n_cont, lin_layer_sizes=[200, 100], output_size=1, emb_dropout=0.3, lin_layer_dropouts=[0.1,0.1]).to(device)
 
-no_of_epochs = 50
+model = Network(emb_dims, no_of_cont=n_cont, lin_layer_sizes=[100, 50], emb_dropout=0.2, lin_layer_dropouts=[0.1, 0.1], in_channels=4, out_channels=20, kernel_size=12, RNN_hidden_size=15, RNN_layers=1, last_lin_size=25).to(device)
 
-#criterion = nn.MSELoss()
+no_of_epochs = 5
+
+# Loss function
 criterion = torch.nn.BCELoss()
 #criterion = torch.nn.NLLLoss()
 #criterion = torch.nn.BCEWithLogitsLoss()
@@ -120,14 +102,21 @@ criterion = torch.nn.BCELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
 
 for epoch in range(no_of_epochs):
-    for y, cont_x, cat_x in dataloader:
+    
+    model.train()
+    
+    for y, cont_x, cat_x, distal_x in dataloader:
         cat_x = cat_x.to(device)
         cont_x = cont_x.to(device)
+        distal_x = distal_x.to(device)
         y  = y.to(device)
         
         # Forward Pass
         #preds = model(cont_x, cat_x) #original
-        preds = model.forward(cont_x, cat_x)
+        preds = model.forward((cont_x, cat_x), distal_x)
+        #print("preds:")
+        #print(preds.shape)
+        
         loss = criterion(preds, y)
         
         # Backward Pass and Optimization
@@ -135,27 +124,45 @@ for epoch in range(no_of_epochs):
         loss.backward()
         optimizer.step()
     
-    #print([preds[0:20], y[0:20]])
-    #fpr, tpr, thresholds = metrics.roc_curve(y, preds, pos_label=1)
-    #scaled_model = ModelWithTemperature(model)
-    #scaled_model.set_temperature(dataloader1)
-
-    #pred_y = scaled_model.forward(test_cont_x, test_cat_x)
-    pred_y = model.forward(test_cont_x, test_cat_x)
+    model.eval()
+    pred_y = torch.empty(0, 1).to(device)
+    test_y = torch.empty(0, 1).to(device)
     
-    ece_model = _ECELoss(10)
-    ece_model.zero_grad()
+    with torch.no_grad():
+        for y, cont_x, cat_x, distal_x in dataloader1:
+            cat_x = cat_x.to(device)
+            cont_x = cont_x.to(device)
+            distal_x = distal_x.to(device)
+            y  = y.to(device)
+        
+            # Forward Pass
+            #preds = model(cont_x, cat_x) #original
+            preds = model.forward((cont_x, cat_x), distal_x)
+            pred_y = torch.cat((pred_y, preds), dim=0)
+            test_y = torch.cat((test_y, y), dim=0)
+        
 
-    #check flanking k-mers
-    #pd.Series
-    #tmp = pred_y.cpu().detach().numpy()
-    #print(tmp)
+    #pred_y = model.forward((test_cont_x, test_cat_x), test_distal_x)
+
     y_prob = pd.Series(data=to_np(pred_y).T[0], name="prob")    
-    data_and_prob = pd.concat([data_test, y_prob], axis=1)
+    data_and_prob = pd.concat([data_local_test, y_prob], axis=1)
 
-    all_pred_y = model.forward(all_cont_x, all_cat_x)
+    torch.cuda.empty_cache()
+    #all_pred_y = model.forward((all_cont_x, all_cat_x), all_distal_x)
+    all_pred_y = torch.empty(0, 1).to(device)
+    with torch.no_grad():
+        for y, cont_x, cat_x, distal_x in dataloader2:
+            cat_x = cat_x.to(device)
+            cont_x = cont_x.to(device)
+            distal_x = distal_x.to(device)
+            y  = y.to(device)
+        
+            preds = model.forward((cont_x, cat_x), distal_x)
+            all_pred_y = torch.cat((all_pred_y, preds), dim=0)
+
+        
     all_y_prob = pd.Series(data=to_np(all_pred_y).T[0], name="prob")
-    all_data_and_prob = pd.concat([data, all_y_prob], axis=1)
+    all_data_and_prob = pd.concat([data_local, all_y_prob], axis=1)
 
     print ('3mer correlation - test: ' + str(f3mer_comp(data_and_prob)))
     print ('3mer correlation - all: ' + str(f3mer_comp(all_data_and_prob)))
@@ -172,9 +179,11 @@ for epoch in range(no_of_epochs):
     
     brier_score = metrics.brier_score_loss(to_np(test_y), to_np(pred_y))
     test_pred = to_np(torch.cat((test_y,pred_y),1))
-    logits = torch.cat((1-pred_y,pred_y),1)
-    logits = torch.log(logits/(1-logits))
+    
+    #logits = torch.cat((1-pred_y,pred_y),1)
+    #logits = torch.log(logits/(1-logits))
     #ECE = to_np(ece_model.forward(logits, test_y.long()))
+    
     prob_true, prob_pred = calibration.calibration_curve(to_np(test_y), to_np(pred_y),n_bins=50)
     
     #print("calibration: ", np.column_stack((prob_pred,prob_true)))
